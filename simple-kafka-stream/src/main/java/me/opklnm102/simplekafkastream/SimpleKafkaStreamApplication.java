bcc166da0ffe0f476ed5ef7770700827344f6ea1
Hue Kim <opklnm102@gmail.com>
2020-09-17 00:49:09 +0900

    start kafka stream

---
+++ simple-kafka-stream/src/main/java/me/opklnm102/simplekafkastream/SimpleKafkaStreamApplication.java
@@ -0,0 +1,13 @@
+package me.opklnm102.simplekafkastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKafkaStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleKafkaStreamApplication.class, args);
+    }
+
+}
---
+++ simple-kafka-stream/src/test/java/me/opklnm102/simplekafkastream/SimpleKafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package me.opklnm102.simplekafkastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
